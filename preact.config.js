a8aa6758dfffe060846d510ba435bac72cbc5c64
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-10-17 20:18:57 -0500

    webpack config to inject environment variables into process.env constants

--- preact.config.js
+++ preact.config.js
@@ -1,16 +1,24 @@
 const { parsed } = require("dotenv-safe").config();
+const { DefinePlugin } = require("webpack");
 
 export default function (config, env, helpers) {
-  // dotenv injection
-  const { plugin } = helpers.getPluginsByName(config, "DefinePlugin")[0];
-  Object.assign(
-    plugin.definitions,
-    Object.keys(parsed).reduce(
-      (env, key) => ({
-        ...env,
-        [`process.env.${key}`]: JSON.stringify(parsed[key]),
-      }),
-      {}
-    )
+  // dotenv injection, local development
+  const dotEnvKeys = Object.keys(parsed).reduce(
+    (env, key) => ({
+      ...env,
+      [`process.env.${key}`]: JSON.stringify(parsed[key]),
+    }),
+    {}
   );
+
+  // process.env injection, production (Netlify)
+  const processEnvKeys = Object.keys(process.env).reduce(
+    (env, key) => ({
+      ...env,
+      [`process.env.${key}`]: JSON.stringify(process.env[key]),
+    }),
+    {}
+  );
+
+  config.plugins.push(new DefinePlugin({ ...processEnvKeys, ...dotEnvKeys }));
 }
--- src/components/app.js
+++ src/components/app.js
@@ -3,26 +3,30 @@ import { h } from "preact";
 // Code-splitting is automated for `routes` directory
 import Home from "../routes/home";
 
-const App = () => (
-  <div id="app" className="app">
-    <div>
-      <h1>Trick or treat it forward</h1>
-      <h2>Neighbors helping neighbors</h2>
-      <p>
-        Instead of passing out treats this year, we're making a $
-        {process.env.DONATION_PER_PERSON} donation to{" "}
-        <a href={process.env.DONATING_TO_WEBSITE}>{process.env.DONATING_TO}</a>{" "}
-        (up to ${process.env.DONATION_MAX})
-      </p>
+const App = () => {
+  return (
+    <div id="app" className="app">
+      <div>
+        <h1>Trick or treat it forward</h1>
+        <h2>Neighbors helping neighbors</h2>
+        <p>
+          Instead of passing out treats this year, we're making a $
+          {`${process.env.DONATION_PER_PERSON}`} donation to{" "}
+          <a
+            href={`${process.env.DONATING_TO_WEBSITE}`}
+          >{`${process.env.DONATING_TO}`}</a>{" "}
+          (up to ${`${process.env.DONATION_MAX}`})
+        </p>
+      </div>
+      <Home path="/" />
+      <footer>
+        <small>
+          Built with ♡ |{" "}
+          <a href="https://github.com/mercedesb/trick-or-treat">Github</a>
+        </small>
+      </footer>
     </div>
-    <Home path="/" />
-    <footer>
-      <small>
-        Built with ♡ |{" "}
-        <a href="https://github.com/mercedesb/trick-or-treat">Github</a>
-      </small>
-    </footer>
-  </div>
-);
+  );
+};
 
 export default App;
